27a8d27d0a4fcf78252045b009ae8bbbc56b1acc
lalchan123 <lalchanbadsa12345@gmail.com>
2024-11-08 08:34:28 -0600

    lalchan committed

--- components/MainNavbar.jsx
+++ components/MainNavbar.jsx
@@ -106,7 +106,7 @@ const MainNavbar = () => {
                     <span class="sr-only">Search</span>
                 </button>
               </div>
-              <div class="relative w-6 h-6 ml-[20px] overflow-hidden bg-gray-100 rounded-lg dark:bg-gray-600">
+              <div class="relative w-6 h-6 ml-[30px] overflow-hidden bg-gray-100 rounded-lg dark:bg-gray-600">
                 <svg class="absolute w-4 h-4 text-gray-400 left-1 mt-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
               </div>
               <div class="relative w-6 h-6 ml-[8px] overflow-hidden bg-gray-100 rounded-lg dark:bg-gray-600">
